7bfda8401596746b4260d97bbb5b8d2f69ae62df
Ethan Garber <ethan.garber@parivedasolutions.com>
2017-06-20 09:44:25 -0400

    text boxes now interacting with state

--- code/src/routes/Home/components/HomeView.js
+++ code/src/routes/Home/components/HomeView.js
@@ -1,26 +1,86 @@
 import React from 'react'
 import classes from './HomeView.scss'
 
-export const HomeView = () => (
-  <div>
-    <h2>Create your pod-racer!</h2>
-    <div className={classes.nameEntry}>
-      <div>Pilot Name</div>
-      <div><input className={classes.nameInput} /></div>
-    </div>
-    <div className={classes.nameEntry}>
-      <div>Ship Name</div>
-      <div><input className={classes.nameInput} /></div>
-    </div>
-    <div className={classes.nameEntry}>
-      <div>Ship Max Speed</div>
-      <div><input className={classes.nameInput} /></div>
-    </div>
-    <div className={classes.nameEntry}>
-      <div>Ship Acceleration</div>
-      <div><input className={classes.nameInput} /></div>
-    </div>
-  </div>
-)
+class HomeView extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      pilotName: '',
+      shipName: '',
+      shipMaxSpeed: 0,
+      shipAcceleration: 0
+    }
+
+    this.setPilotName = this.setPilotName.bind(this)
+  }
+
+  setPilotName (e) {
+    this.setState({
+      pilotName: e.target.value
+    }, () => setTimeout(console.log(this.state), 1000))
+  }
+
+  setShipMaxSpeed (e) {
+    this.setState({
+      shipMaxSpeed: e.target.value
+    })
+  }
+
+  setShipAcceleration (e) {
+    this.setState({
+      shipAcceleration: e.target.value
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <h2>Create your pod-racer!</h2>
+        <div className={classes.nameEntry}>
+          <div>Pilot Name</div>
+          <div>
+            <input
+              className={classes.nameInput}
+              value={this.state.pilotName}
+              onChange={this.setPilotName}
+            />
+          </div>
+        </div>
+        <div className={classes.nameEntry}>
+          <div>Ship Name</div>
+          <div>
+            <input
+              className={classes.nameInput}
+              value={this.state.shipName}
+              onChange={(e) => this.setState({ shipName: e.target.value })}
+            />
+          </div>
+        </div>
+        <div className={classes.nameEntry}>
+          <div>Ship Max Speed</div>
+          <div>
+            <input
+              className={classes.nameInput}
+              value={this.state.shipMaxSpeed}
+              onChange={this.setShipMaxSpeed}
+            />
+          </div>
+        </div>
+        <div className={classes.nameEntry}>
+          <div>Ship Acceleration</div>
+          <div>
+            <input
+              className={classes.nameInput}
+              value={this.state.shipAcceleration}
+              onChange={this.setShipAcceleration}
+            />
+          </div>
+        </div>
+      </div>
+
+    )
+  }
+}
 
 export default HomeView
